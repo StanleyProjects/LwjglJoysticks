59ce224616958d8bb64f72491670cfc3b7384269
Stanley Wintergreen <kepocnhh@gmail.com>
2024-02-03 16:17:04 +0300

    check readme

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # LwjglJoysticks
 A few Lwjgl joysticks extensions.
+
+---
+
+## Unstable
+
+![version](https://img.shields.io/static/v1?label=version&message=0.0.2-UNSTABLE&labelColor=212121&color=2962ff&style=flat)
+
+- [Maven](https://s01.oss.sonatype.org/content/repositories/snapshots/com/github/kepocnhh/LwjglJoysticks/0.0.2-UNSTABLE)
+
+### Build
+```
+$ gradle lib:assembleUnstableJar
+```
+
+### Import
+```kotlin
+repositories {
+    maven("https://s01.oss.sonatype.org/content/repositories/snapshots")
+}
+
+dependencies {
+    implementation("com.github.kepocnhh:LwjglJoysticks:0.0.2-UNSTABLE")
+}
+```
--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,4 +1,24 @@
+import sp.gx.core.Badge
+import sp.gx.core.GitHub
+import sp.gx.core.Markdown
+import sp.gx.core.Maven
+import sp.gx.core.camelCase
+import sp.gx.core.check
+import sp.gx.core.colonCase
+import sp.gx.core.kebabCase
+import java.util.Locale
+
 version = "0.0.2"
+
+val maven = Maven.Artifact(
+    group = "com.github.kepocnhh",
+    id = rootProject.name,
+)
+
+val gh = GitHub.Repository(
+    owner = "StanleyProjects",
+    name = rootProject.name,
+)
 
 repositories.mavenCentral()
 
@@ -9,7 +29,7 @@ plugins {
 val compileKotlinTask = tasks.getByName<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>("compileKotlin") {
     kotlinOptions {
         jvmTarget = Version.jvmTarget
-//        freeCompilerArgs = freeCompilerArgs + setOf("-module-name", colonCase(maven.group, maven.id)) // todo
+        freeCompilerArgs = freeCompilerArgs + setOf("-module-name", colonCase(maven.group, maven.id))
     }
 }
 
@@ -26,5 +46,34 @@ dependencies {
     implementation(platform("$group:lwjgl-bom:${Version.lwjgl}"))
     LwjglUtil.modules.forEach { name ->
         implementation(group = group, name = name)
+    }
+}
+
+"unstable".also { variant ->
+    val version = kebabCase(version.toString(), variant.uppercase(Locale.US))
+    task(camelCase("check", variant, "Readme")) {
+        doLast {
+            val badge = Markdown.image(
+                text = "version",
+                url = Badge.url(
+                    label = "version",
+                    message = version,
+                    color = "2962ff",
+                ),
+            )
+            val expected = setOf(
+                badge,
+                Markdown.link("Maven", Maven.Snapshot.url(maven, version)),
+                "implementation(\"${colonCase(maven.group, maven.id, version)}\")",
+            )
+            val report = layout.buildDirectory.get()
+                .dir("reports/analysis/readme")
+                .file("index.html")
+                .asFile
+            rootDir.resolve("README.md").check(
+                expected = expected,
+                report = report,
+            )
+        }
     }
 }
